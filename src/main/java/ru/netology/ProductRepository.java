df42936f47c660c2b807d200570d5fc593e2c626
Toybear <50303627+Toybear1986@users.noreply.github.com>
2022-08-25 17:41:39 +0300

    [AlreadyExistsException for save] (#2)
    * NotFoundException for removeById
    
    * AlreadyExistsException for save

--- src/main/java/ru/netology/ProductRepository.java
+++ src/main/java/ru/netology/ProductRepository.java
@@ -4,6 +4,13 @@ public class ProductRepository {
   protected Product[] products = new Product[0];
 
   public void save(Product product) {
+    for (Product newProduct :products) {
+      if (newProduct.getId() == product.id) {
+        throw new AlreadyExistsException(
+                "Product with required id " + product.id + " already exist"
+        );
+      }
+    }
     Product[] tmp = new Product[products.length + 1];
     System.arraycopy(products, 0, tmp, 0, products.length);
     tmp[tmp.length - 1] = product;
@@ -14,7 +21,21 @@ public class ProductRepository {
     return products;
   }
 
+  public Product findById(int id) {
+    for (Product product : products) {
+      if (id == product.getId()) {
+        return product;
+      }
+    }
+    return null;
+  }
+
   public void removeById(int id) {
+    if (findById(id) == null) {
+      throw new NotFoundException(
+              "Element with id: " + id + " not found"
+      );
+    }
     Product[] tmp = new Product[products.length - 1];
     int copyToIndex = 0;
     for (Product product : products) {
--- src/test/java/ProductRepositoryTest.java
+++ src/test/java/ProductRepositoryTest.java
@@ -1,20 +1,13 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import ru.netology.Book;
-import ru.netology.Product;
-import ru.netology.ProductRepository;
-import ru.netology.Smartphone;
-
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.doReturn;
+import ru.netology.*;
 
 public class ProductRepositoryTest {
 
-  ProductRepository mock = Mockito.mock(ProductRepository.class);
   Product product = new Product(1, "Не книга и не смартфон", 1_000);
   Book book = new Book(2, "Самая интересная книга", 2_000, "Незнайка");
   Smartphone smartphone = new Smartphone(3, "Xiaomi Mi 8 Pro", 10_000, "Xiaomi");
+  Product newProduct = new Product(4, "Тест", 123);
 
   @Test
   public void test() {
@@ -35,9 +28,31 @@ public class ProductRepositoryTest {
   }
 
   @Test
-  public void NullException() {
-    doReturn(null).when(mock).findAll(); //Пробуем найти продукты в пустом массиве
-    doNothing().when(mock).removeById(4); // Пробуем удалить из пустого массива
-  }
+  public void testExceptions() {
+    ProductRepository repo = new ProductRepository();
+    //Добавление
+    repo.save(product);
+    repo.save(book);
+    repo.save(smartphone);
 
+    //Проверяем удаление продукта по Id, чтобы убедиться, что все работает после правок
+    repo.removeById(1);
+    Product[] expectedWithoutRemoved = {book, smartphone};
+    Assertions.assertArrayEquals(expectedWithoutRemoved, repo.findAll());
+
+    //Проверяем эксепшн при значениее несуществующем (удаленном в первом случае) в массиве
+    Assertions.assertThrows(NotFoundException.class, () -> repo.removeById(1));
+
+    //Проверяем эксепшн при отрицательном значении
+    Assertions.assertThrows(NotFoundException.class, () -> repo.removeById(-100));
+
+    //Проверяем добавление нового продукта
+    repo.save(newProduct);
+    Product[] expectedArray = {book, smartphone, newProduct};
+    Assertions.assertArrayEquals(expectedArray, repo.findAll());
+
+    //Проверяем эксепшн при добавлении нового продукта с существующим id
+    Assertions.assertThrows(AlreadyExistsException.class, () -> repo.save(newProduct));
+
+  }
 }
